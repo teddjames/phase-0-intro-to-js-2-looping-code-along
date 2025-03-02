696764b71fd56a3191ac1c8f912c22efe9659ed1
Tedd James <tedd.james@student.moringaschool.com>
2025-03-02 23:27:38 +0300

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return messages;
+}
+
+const names = ["Alice", "Bob", "Charlie"];
+const event = "birthday";
+console.log(writeCards(names, event));
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+
+countDown(10);
